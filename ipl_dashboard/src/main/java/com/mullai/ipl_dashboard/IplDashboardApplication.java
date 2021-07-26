3efcdb6b6d2a68d745cb279d57c226715403dff0
MullaiArgent <mullairajan2000@gmail.com>
2021-07-26 23:53:42 +0530

    Initial commit

---
+++ ipl_dashboard/src/main/java/com/mullai/ipl_dashboard/IplDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.mullai.ipl_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardApplication.class, args);
+	}
+
+}
---
+++ ipl_dashboard/src/test/java/com/mullai/ipl_dashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mullai.ipl_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
